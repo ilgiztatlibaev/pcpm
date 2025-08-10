93f0ad040a5447dc02a1aef6dd065048a328f5f8
Ilgiz Tatlibaev <ilgiztatlibaev@gmail.com>
2025-08-10 18:46:02 +0300

    fix: use POSTGRES_URL explicitly in _db.mjs

--- api/_db.mjs
+++ api/_db.mjs
@@ -1,2 +1,9 @@
-import { sql } from '@vercel/postgres';
-export { sql };
+import { createClient } from '@vercel/postgres';
+
+const client = createClient({
+  connectionString: process.env.POSTGRES_URL,
+});
+
+await client.connect();
+
+export const sql = client.sql.bind(client);
